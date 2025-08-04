da2ecb5117a95117e6dec47d25aa6db82234f357
William <williamliususanto@gmail.com>
2025-08-04 09:54:02 +0700

    feat: add cards & rating star

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Star from "./components/Star";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex justify-center items-center h-lvh bg-gray-800">
+      <div className="rounded-lg shadow-md p-6 bg-white">
+        <h1 className="font-bold text-2xl text-center">Rating</h1>
+
+        <form action="" className="mt-4">
+          <div className="flex justify-center gap-3">
+            {Array.from({ length: 5 }, (_, index: number) => (
+              <Star key={index}></Star>
+            ))}
+          </div>
+
+          <div className="w-full flex justify-center mt-10">
+            <button type="submit" className="rounded-lg bg-blue-400 px-3 py-2">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Star.tsx
@@ -0,0 +1,42 @@
+interface StarProps {
+  fillColor?: string;
+}
+
+const Star = ({ fillColor }: StarProps) => {
+  const defaultFill: string = "#FFFFFF";
+  const defaultBorder: string = "#3153FF";
+
+  return (
+      <svg
+        className="h-12"
+        viewBox="0 -0.5 33 33"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title>star</title>
+        <defs></defs>
+        <g
+          id="Vivid.JS"
+          stroke={defaultBorder}
+          strokeWidth="1"
+          fillRule="evenodd"
+        >
+          <g
+            id="Vivid-Icons"
+            transform="translate(-903.000000, -411.000000)"
+            fill={fillColor ?? defaultFill}
+          >
+            <g id="Icons" transform="translate(37.000000, 169.000000)">
+              <g id="star" transform="translate(858.000000, 234.000000)">
+                <g transform="translate(7.000000, 8.000000)" id="Shape">
+                  <polygon points="27.865 31.83 17.615 26.209 7.462 32.009 9.553 20.362 0.99 12.335 12.532 10.758 17.394 0 22.436 10.672 34 12.047 25.574 20.22"></polygon>
+                </g>
+              </g>
+            </g>
+          </g>
+        </g>
+      </svg>
+  );
+};
+
+export default Star;
